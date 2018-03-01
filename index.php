c46b0844d65c34cb5be963d8843334cdfe5bcc07
Nena Ani <nenaani@192.168.1.224>
2018-03-01 16:08:51 +0100

    rekenmachine

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$getal1 = $_GET['getal1'];
+echo " ".$getal1;
+$getal2 = $_GET['getal2'];
+echo " + ".$getal2;
+
+optellen($getal1,$getal2);
+
+function optellen ($getala,$getalb){
+    
+    $totaal = $getala + $getalb;
+    
+    echo " = ".$totaal;
+    
+}
+       
+   
+?>
